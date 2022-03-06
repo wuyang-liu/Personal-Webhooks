ad883a05aaf20a51b9cc22a37cd8bbb9146eac17
wuyang-liu <liuwuyayui@gmail.com>
2022-03-05 19:22:23 -0800

    feat: Enable All CORS Requests

--- routes/proxy.js
+++ routes/proxy.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 var router = express.Router();
 
 // request for json
-router.get('/', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   let {url} = req.body;
   const data = (await axios.get(url)).data;
   console.log("in proxy.js: ", data);
